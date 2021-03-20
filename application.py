a85b0e5bb2128b7cbabd54860568487f609263b1
DESKTOP-3GDP90J <ogulcan.ertunc01@icatt.it>
2021-03-20 22:26:23 +0100

    Try to fix

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 import nltk
 import streamlit as sl
 nltk.download('punkt')
-from Helpers.helpers import *
 import torch
 import textwrap
 wrapper = textwrap.TextWrapper(width=70)
@@ -10,6 +9,14 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 stop_words = stopwords.words('english')
 torch.manual_seed(SEED)
+import transformers
+from transformers import GPT2Tokenizer, GPT2DoubleHeadsModel
+import nltk
+from nltk.tokenize import word_tokenize
+nltk.download('stopwords')
+nltk.download('punkt')
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
 
 #===============================================================================================#
 
@@ -18,17 +25,14 @@ torch.manual_seed(SEED)
 #===============================================================================================#
 
 sl.title("Abstractive Summarizator by Ogi")
+
 title = sl.text_area('Enter Your Title Here')
 text = sl.text_area('Enter Your Text Here ')
-
 #===============================================================================================#
 
 # Functions and Models Prepared
 
 #===============================================================================================#
-
-
-
 
 GPT2_config_directory = 'GPT2_dir'
 tokenizer_GPT2 = GPT2Tokenizer.from_pretrained(GPT2_config_directory)
@@ -36,8 +40,14 @@ special_tokens = {'bos_token': '<|startoftext|>', 'eos_token': '<|endoftext|>', 
                   'additional_special_tokens': ['<|keyword|>', '<|summarize|>']}
 tokenizer_GPT2.add_special_tokens(special_tokens)
 GPT2_generator = GPT2DoubleHeadsModel.from_pretrained(GPT2_config_directory)
-
 use_GPU_GPT_generator = False
+def get_keywords(text_):
+    some_text = text_
+    lowered = some_text.lower()
+    tokens = nltk.tokenize.word_tokenize(lowered)
+    keywords = [keyword for keyword in tokens if keyword.isalpha() and not keyword in stop_words]
+    keywords_string = ','.join(keywords)
+    return keywords_string
 list_keywords = get_keywords(text)
 
 GPT2_input = tokenizer_GPT2.encode('<|startoftext|> ' +title + list_keywords + ' <|summarize|> ')
@@ -45,8 +55,14 @@ GPT2_input_torch = torch.tensor(GPT2_input, dtype=torch.long)
 print("the keyword input :")
 wrapper.wrap(tokenizer_GPT2.decode(GPT2_input_torch))
 
+temperature = 1
+greedy_search = False
+top_k = 35
+top_p = 0.8
+max_length = 200
+min_length = 20
+num_return_sequences = 3
 
-temperature, greedy_search, top_k, top_p, max_length, min_length, num_return_sequences = get_params()
 
 if use_GPU_GPT_generator:
     GPT2_generator = GPT2_generator.to(device)
@@ -57,8 +73,17 @@ if do_sample == False:
     num_return_sequences = 1
 
 
-sampling_output = sampling_output_func(GPT2_generator,GPT2_input_torch, temperature, greedy_search, top_k, top_p, max_length, min_length, num_return_sequences,do_sample)
-
+sampling_output = GPT2_generator.generate(
+        input_ids=GPT2_input_torch.unsqueeze(0),
+        max_length=max_length + len(GPT2_input_torch),
+        min_length=min_length + len(GPT2_input_torch),
+        temperature=temperature,
+        decoder_start_token_id='<|summarize|>',
+        top_k=top_k,
+        top_p=top_p,
+        do_sample=do_sample,
+        num_return_sequences=num_return_sequences,
+        no_repeat_ngram_size=3)
 #===============================================================================================#
 # Summary
 #===============================================================================================#
